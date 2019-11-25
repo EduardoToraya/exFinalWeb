904383e2d58011ae45006b2db167b3148d22015c
Eduardo Alejandro Toraya Solís <eduar_ts1997@hotmail.com>
2019-11-25 17:15:30 -0600

    <
    a

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // revisa tu connectionURL aqui :-)
 const config = require("../config.js")
-const connectionURL =  process.env.connectionURL || 
+const connectionURL =  process.env.DB_CONNECTION_STRING || 
 	config.connectionURL 
 
 
